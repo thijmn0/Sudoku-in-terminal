1e68ebb0b80ece6b7494063af9079848476d6103
Thijmen Rikkert <101828986+thijmn0@users.noreply.github.com>
2023-04-18 20:55:15 +0000

    added the input of a file

--- myEditor.c
+++ myEditor.c
@@ -7,7 +7,7 @@
 #include <stdint.h>
 #include <string.h>
 // https://stackoverflow.com/a/826027
-
+// Debug with valgrind if ever needed: gcc -g -O1 myEditor.c -o myEditor;	valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -s ./myEditor
 
 
 static struct termios orig_termios;
@@ -82,66 +82,14 @@ void abFree(struct abuf *ab) {
     free(ab->b);
 }
 
-void print_large_sudoku(uint8_t sudoku[81]){
-	const uint8_t num_len[]={7,5,9,9,9,9,9,9,9,7,9,5,9,9,9,9,9,9,9,5,5,9,9,9,9,9,9};
-	const char* num[]={"▗▄ "," █ ","▗█▖","▗▄▖","▗▄▌","▐▄▖","▗▄▖","▗▄▌","▗▄▌","▄ ▄","▜▃█","  █","▗▄▖","▐▄▖","▗▄▌","▄▄▖","▙▄▖","▙▄▌","▄▄▖","  ▌","  ▌","▄▄▖","▙▄▌","▙▄▌","▄▄▖","▙▄▌","▄▄▌"};
-	// ▗▄  ▗▄▖ ▗▄▖ ▄ ▄ ▗▄▖ ▄▄▖ ▄▄▖ ▄▄▖ ▄▄▖
-	//  █  ▗▄▌ ▗▄▌ ▜▃█ ▐▄▖ ▙▄▖   ▌ ▙▄▌ ▙▄▌
-	// ▗█▖ ▐▄▖ ▗▄▌   █ ▗▄▌ ▙▄▌   ▌ ▙▄▌ ▄▄▌
-	struct abuf ab=ABUF_INIT;
-	abAppend(&ab,"\x1b[?25l",6);
-	abAppend(&ab,"\x1b[H",3);
-    abAppend(&ab,"┏━━━━━━━┯━━━━━━━┯━━━━━━━┳━━━━━━━┯━━━━━━━┯━━━━━━━┳━━━━━━━┯━━━━━━━┯━━━━━━━┓\r\n┃  ",226);//8*9*3+3+2+10*9+3+2+3
-    for(uint8_t i=0,j=0;i<81;i++){
-    	if(*(sudoku+i)==0){
-    		abAppend(&ab,"   ",3);
-    	}
-    	else{
-    		uint8_t p=(*(sudoku+i)-1)*3+j;
-    		abAppend(&ab,num[p],num_len[p]);
-    	}
-    	if(i%9==8){
-    		if(j!=2){
-    			i-=9;
-    			j++;
-    			abAppend(&ab,"  ┃\r\n┃  ",12);
-    		}else{
-    			j=0;
-	    		if(i!=80){
-	    			if(i%27==26){
-	    				abAppend(&ab,"  ┃\r\n┣━━━━━━━┿━━━━━━━┿━━━━━━━╋━━━━━━━┿━━━━━━━┿━━━━━━━╋━━━━━━━┿━━━━━━━┿━━━━━━━┫\r\n┃  ",233);//97*2+3+219 
-	    			}
-	    			else{
-						abAppend(&ab,"  ┃\r\n┣╶╶╶╶╶╶╶├╶╶╶╶╶╶╶├╶╶╶╶╶╶╶┠╶╶╶╶╶╶╶├╶╶╶╶╶╶╶├╶╶╶╶╶╶╶┠╶╶╶╶╶╶╶├╶╶╶╶╶╶╶├╶╶╶╶╶╶╶┃\r\n┃  ",233);
-	    			}
-	    		}
-	    		else{
-	    			abAppend(&ab,"  ┃\r\n┗━━━━━━━┷━━━━━━━┷━━━━━━━┻━━━━━━━┷━━━━━━━┷━━━━━━━┻━━━━━━━┷━━━━━━━┷━━━━━━━┛\r\n",228);
-	    		}
-    		}
-    	}
-    	else if(i%3==2){
-    		abAppend(&ab,"  ┃  ",7);
-    	}
-    	else{
-    		abAppend(&ab,"  ╎  ",7);
-    	}
-    }
-	abAppend(&ab,"\x1b[?25h",6);
-    if(write(STDOUT_FILENO,ab.b,ab.len)){
-    	abFree(&ab);
-    	exit(1);
-    }
-    abFree(&ab);
-}
-
-void print_large_generated_sudoku(uint8_t sudoku[81]){
+void print_large_sudoku(const uint8_t sudoku[81]){
 	///im lazy, ill improve it later
 	const uint8_t num_len[]={7,5,9,9,9,9,9,9,9,7,9,5,9,9,9,9,9,9,9,5,5,9,9,9,9,9,9};
 	const char* num[]={"▗▄ "," █ ","▗█▖","▗▄▖","▗▄▌","▐▄▖","▗▄▖","▗▄▌","▗▄▌","▄ ▄","▜▃█","  █","▗▄▖","▐▄▖","▗▄▌","▄▄▖","▙▄▖","▙▄▌","▄▄▖","  ▌","  ▌","▄▄▖","▙▄▌","▙▄▌","▄▄▖","▙▄▌","▄▄▌"};
 	// ▗▄  ▗▄▖ ▗▄▖ ▄ ▄ ▗▄▖ ▄▄▖ ▄▄▖ ▄▄▖ ▄▄▖
 	//  █  ▗▄▌ ▗▄▌ ▜▃█ ▐▄▖ ▙▄▖   ▌ ▙▄▌ ▙▄▌
 	// ▗█▖ ▐▄▖ ▗▄▌   █ ▗▄▌ ▙▄▌   ▌ ▙▄▌ ▄▄▌
+
 	struct abuf ab=ABUF_INIT;
 	abAppend(&ab,"\x1b[?25l",6);
 	abAppend(&ab,"\x1b[H",3);
@@ -182,14 +130,13 @@ void print_large_generated_sudoku(uint8_t sudoku[81]){
     	}
     }
 	abAppend(&ab,"\x1b[?25h",6);
-    if(write(STDOUT_FILENO,ab.b,ab.len)){
-    	abFree(&ab);
-    	exit(1);
+    if(write(STDOUT_FILENO,ab.b,ab.len)==-1){
+    	//something went wrong
     }
     abFree(&ab);
 }
 
-void print_small_sudoku(uint8_t sudoku[81]){
+void print_small_sudoku(const uint8_t sudoku[81]){
 	//snprintf maybe
 	struct abuf ab=ABUF_INIT;
 	abAppend(&ab,"\x1b[?25l",6);
@@ -224,9 +171,8 @@ void print_small_sudoku(uint8_t sudoku[81]){
     	}
     }
     abAppend(&ab,"\x1b[?25h",6);
-    if(write(STDOUT_FILENO,ab.b,ab.len)){
-    	abFree(&ab);
-    	exit(1);
+    if(write(STDOUT_FILENO,ab.b,ab.len)==-1){
+    	//something went wrong
     }
     abFree(&ab);
 }
@@ -241,13 +187,25 @@ _Bool process_key(void){
 }
 
 int main(int argc, char **argv){
+	uint8_t grid[]={0,6,9,0,5,0,3,0,0,0,8,1,0,9,3,0,0,5,0,0,5,4,8,0,0,1,0,9,2,6,0,0,0,7,0,8,0,5,0,0,0,0,0,4,9,0,0,0,0,0,9,6,0,1,0,0,4,0,3,8,0,2,7,0,0,0,0,4,5,0,0,0,5,1,0,2,7,6,8,0,4};
 	if(system("clear")){
 		return -1;
 	}
+	if(argc==2){
+		FILE* fp=fopen(argv[1],"r");
+		if(!fp){
+			printf("no file named found\n");
+			return 0;
+		}
+		for(int i=0;i<81;i++){
+			grid[i]=(uint8_t)fgetc(fp)-48;
+		}
+		fclose(fp);
+	}
 	E.raw_mode=0;
 	enable_raw_mode(STDIN_FILENO);
-	uint8_t grid[]={0,6,9,0,5,0,3,0,0,0,8,1,0,9,3,0,0,5,0,0,5,4,8,0,0,1,0,9,2,6,0,0,0,7,0,8,0,5,0,0,0,0,0,4,9,0,0,0,0,0,9,6,0,1,0,0,4,0,3,8,0,2,7,0,0,0,0,4,5,0,0,0,5,1,0,2,7,6,8,0,4};
-	print_small_sudoku(grid);
+	//const uint8_t grid[]={0,6,9,0,5,0,3,0,0,0,8,1,0,9,3,0,0,5,0,0,5,4,8,0,0,1,0,9,2,6,0,0,0,7,0,8,0,5,0,0,0,0,0,4,9,0,0,0,0,0,9,6,0,1,0,0,4,0,3,8,0,2,7,0,0,0,0,4,5,0,0,0,5,1,0,2,7,6,8,0,4};
+	print_large_sudoku(grid);
 	// write(STDOUT_FILENO,"\033[?1003h",8);
 	while(1){
 		if(process_key()){
@@ -256,7 +214,6 @@ int main(int argc, char **argv){
 	}
 	disable_raw_mode();
 	printf("\033[?1003l");
-	printf("uhm");
 	if(system("clear")){
 		return -1;
 	}
