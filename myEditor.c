6681beed91ece4d2775c1a6c7797ccfa4853887a
Thijmen Rikkert <101828986+thijmn0@users.noreply.github.com>
2023-04-25 12:00:48 +0000

    Update myEditor.c

--- myEditor.c
+++ myEditor.c
@@ -62,6 +62,8 @@ fatal:
 	return -1;
 }
 
+void processKey(
+
 #define ABUF_INIT {NULL,0};
 
 struct abuf{
@@ -204,7 +206,6 @@ int main(int argc, char **argv){
 	}
 	E.raw_mode=0;
 	enable_raw_mode(STDIN_FILENO);
-	//const uint8_t grid[]={0,6,9,0,5,0,3,0,0,0,8,1,0,9,3,0,0,5,0,0,5,4,8,0,0,1,0,9,2,6,0,0,0,7,0,8,0,5,0,0,0,0,0,4,9,0,0,0,0,0,9,6,0,1,0,0,4,0,3,8,0,2,7,0,0,0,0,4,5,0,0,0,5,1,0,2,7,6,8,0,4};
 	print_large_sudoku(grid);
 	// write(STDOUT_FILENO,"\033[?1003h",8);
 	while(1){
